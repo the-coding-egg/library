6e94275a58a23cebc9fc7e668688085de85e7a3b
Dalton McMullen <thecodingegg@gmail.com>
2025-10-12 09:45:28 -0700

    Added books to array
    Able to add the books to the array and display in the console. Next is
    to display them on the page.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 
 function Book(id, name, author, pages, read) {
+  this.id = id;
   this.name = name;
   this.author = author;
   this.pages = pages;
@@ -11,23 +12,26 @@ function Book(id, name, author, pages, read) {
   };
 }
 
-// const hailMary = new Book("Project Hail Mary", "Andy Weir", 496, "read");
-// const creativeAct = new Book("The Creative Act", "Rick Ruben", 300, "read");
-// const mobyDick = new Book("Moby Dick", "Herman Melville", 342, "read");
 // console.log(mobyDick.info());
 // console.log(hailMary.info());
 // console.log(creativeAct.info());
 
 function addBookToLibrary(name, author, pages, read) {
-  let id = randomUUID();
+  let id = self.crypto.randomUUID();
   let newBook = new Book(id, name, author, pages, read);
 
   myLibrary.push(newBook);
 }
 
+addBookToLibrary("Moby Dick", "Herman Melville", 342, "read");
+addBookToLibrary("Project Hail Mary", "Andy Weir", 496, "read");
+addBookToLibrary("The Creative Act", "Rick Ruben", 300, "read");
+
+console.log(myLibrary);
+
 const resetButton = document.querySelector("#resetbutton");
 const submitButton = document.querySelector("#submitbutton");
 
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-});
+// submitButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+// });
