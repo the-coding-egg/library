b2b165e49cd5df1cb364c82c6fc75570a1b35299
Dalton McMullen <thecodingegg@gmail.com>
2025-10-12 10:02:30 -0700

    Displayed array onto HTML
    Need to loop through the books and display the books on each different
    div and make cards for them

--- script.js
+++ script.js
@@ -12,10 +12,6 @@ function Book(id, name, author, pages, read) {
   };
 }
 
-// console.log(mobyDick.info());
-// console.log(hailMary.info());
-// console.log(creativeAct.info());
-
 function addBookToLibrary(name, author, pages, read) {
   let id = self.crypto.randomUUID();
   let newBook = new Book(id, name, author, pages, read);
@@ -23,15 +19,25 @@ function addBookToLibrary(name, author, pages, read) {
   myLibrary.push(newBook);
 }
 
+//add the books to the array
 addBookToLibrary("Moby Dick", "Herman Melville", 342, "read");
 addBookToLibrary("Project Hail Mary", "Andy Weir", 496, "read");
 addBookToLibrary("The Creative Act", "Rick Ruben", 300, "read");
 
+// display the books in the console
 console.log(myLibrary);
 
-const resetButton = document.querySelector("#resetbutton");
-const submitButton = document.querySelector("#submitbutton");
+const resetButton = document.querySelector(".resetbutton");
+const submitButton = document.querySelector(".submitbutton");
 
-// submitButton.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
+// display books onto pages
+// need to run a for loop and display each element in array
+for (let book in myLibrary) {
+  const bookSection = document.querySelector(".book");
+
+  const bookDisplay = document.createElement("div");
+  bookDisplay.classList.add("bookdisplay");
+  bookDisplay.textContent = myLibrary;
+
+  bookSection.appendChild(bookDisplay);
+}
