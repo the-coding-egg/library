49753bb6a83f525a766ec72345d5954c28dd31b6
Dalton McMullen <thecodingegg@gmail.com>
2025-10-12 20:50:54 -0700

    Added cards and started dialog functionality
    Add the books from the array into their own cards. Now started the form
    in a dialog format. Need to understand how to take the submitted form
    and place it onto the screen as another card.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Book(id, name, author, pages, read) {
   this.read = read;
 
   this.info = function () {
-    return `${this.name} by ${this.author}, ${pages}, ${read}.`;
+    return `Name: ${this.name}\nAuthor: ${this.author}\nPages: ${this.pages}\nRead?: ${this.read}`;
   };
 }
 
@@ -20,24 +20,50 @@ function addBookToLibrary(name, author, pages, read) {
 }
 
 //add the books to the array
-addBookToLibrary("Moby Dick", "Herman Melville", 342, "read");
-addBookToLibrary("Project Hail Mary", "Andy Weir", 496, "read");
-addBookToLibrary("The Creative Act", "Rick Ruben", 300, "read");
+addBookToLibrary("Moby Dick", "Herman Melville", 342, "Yes");
+addBookToLibrary("Project Hail Mary", "Andy Weir", 496, "Yes");
+addBookToLibrary("The Creative Act", "Rick Ruben", 300, "Yes");
 
 // display the books in the console
 console.log(myLibrary);
 
-const resetButton = document.querySelector(".resetbutton");
-const submitButton = document.querySelector(".submitbutton");
-
 // display books onto pages
 // need to run a for loop and display each element in array
-for (let book in myLibrary) {
+for (let book of myLibrary) {
   const bookSection = document.querySelector(".book");
 
   const bookDisplay = document.createElement("div");
   bookDisplay.classList.add("bookdisplay");
-  bookDisplay.textContent = myLibrary;
+  bookDisplay.textContent = book.info();
+
+  console.log(book.name);
 
   bookSection.appendChild(bookDisplay);
 }
+
+// be able to create more books
+const resetButton = document.querySelector(".resetbutton");
+const submitButton = document.querySelector(".submitbutton");
+
+submitButton.addEventListener("click", submitNewBook);
+
+function submitNewBook(event) {
+  const bookName = document.querySelector("#name");
+  const bookAuthor = document.querySelector("#author");
+  const bookPages = document.querySelector("#pages");
+  console.log("added book!");
+  addBookToLibrary(bookName, bookAuthor, bookPages, "Yes");
+  event.preventDefault();
+}
+
+const dialogForm = document.querySelector(".dialogform");
+const formButton = document.querySelector(".addbookbutton");
+const cancelButton = document.querySelector(".cancelbutton");
+
+formButton.addEventListener("click", () => {
+  dialogForm.showModal();
+});
+
+cancelButton.addEventListener("click", () => {
+  dialog.close();
+});
