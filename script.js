c45018ab1d54f10322a164c4e06fb04d8cf1dc69
Dalton McMullen <thecodingegg@gmail.com>
2025-10-14 20:47:29 -0700

    Built functionality of delete buttons
    Added the ability to delete all books in the library and the ability to
    delete one book. Also added the ability to add single books.

--- script.js
+++ script.js
@@ -27,26 +27,42 @@ addBookToLibrary("The Creative Act", "Rick Ruben", 300, "No");
 // display the books in the console
 console.log(myLibrary);
 
+const bookSection = document.querySelector(".book");
+
 // display books onto pages
 function displayBooks() {
   for (let book of myLibrary) {
-    const bookSection = document.querySelector(".book");
-
     const bookDisplay = document.createElement("div");
     bookDisplay.classList.add("bookdisplay");
     bookDisplay.textContent = book.info();
 
+    const deleteBook = document.createElement("button");
+    deleteBook.classList.add("deletebook");
+    deleteBook.textContent = "Delete";
+
     console.log(book.name);
 
+    deleteBook.addEventListener("click", () => {
+      bookDisplay.remove();
+    });
+
     bookSection.appendChild(bookDisplay);
+    bookDisplay.appendChild(deleteBook);
   }
 }
 
 // be able to create more books
 const resetButton = document.querySelector(".resetbutton");
 const submitButton = document.querySelector(".submitbutton");
+const clearAllButton = document.querySelector(".clearallbutton");
+
+clearAllButton.addEventListener("click", clearAllBooks);
 
 submitButton.addEventListener("click", submitNewBook);
+
+function clearAllBooks() {
+  bookSection.innerText = "";
+}
 
 function submitNewBook(event) {
   const bookName = document.querySelector("#name").value;
@@ -57,10 +73,15 @@ function submitNewBook(event) {
   addBookToLibrary(bookName, bookAuthor, bookPages, bookRead);
   console.log("added book!");
 
+  document.querySelector("#bookform").reset();
+  dialogForm.close();
+
   console.log(myLibrary);
 
-  displayBooks();
   event.preventDefault();
+
+  clearAllBooks();
+  displayBooks();
 }
 
 const dialogForm = document.querySelector(".dialogform");
@@ -74,3 +95,5 @@ formButton.addEventListener("click", () => {
 cancelButton.addEventListener("click", () => {
   dialogForm.close();
 });
+
+displayBooks();
